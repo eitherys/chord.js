4b8b034e72ebe1a32b07dc322545900a04bc65b2
Nabeel Ansari <na494@drexel.edu>
2014-08-07 18:56:23 +0100

    Attempt #2 on iOS compatibility.

--- chord-app.js
+++ chord-app.js
@@ -199,8 +199,20 @@ function playSound(buffer) {
     sourceNode.start(0);
 }
 
-window.addEventListener('touchstart', function() { source.noteOn(0); }, false);
+window.addEventListener('touchstart', function() {
 
+    // create empty buffer
+    var buffer = myContext.createBuffer(1, 1, 22050);
+    var source = myContext.createBufferSource();
+    source.buffer = buffer;
+
+    // connect to output (your speakers)
+    source.connect(myContext.destination);
+
+    // play the file
+    source.noteOn(0);
+
+}, false);
 
 /* Old Code Dump */
 /*
